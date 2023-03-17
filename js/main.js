e3e37d27070ba167a9cc51f077501c94ebd808a2
Alejandra <amzita@scl.edu.gt>
2023-03-16 23:11:02 -0600

    revisión para la pagina

---
+++ js/main.js
@@ -0,0 +1,34 @@
+function cargar_bloques(){
+
+    let main = document.querySelector(".main");
+    
+    for (let i = 0; i <= 14; i++){
+    
+    let div = document.createElement("div");
+    div.innerHTML = '<div class="bloque"></div>';
+    main.appendChild(div);
+    
+    }
+    
+    }
+    
+    cargar_bloques();
+    
+    function agregar_eventos(){
+    let todo_los_bloques = document.querySelectorAll(".bloque")
+    
+    function saludar(){
+    this.classList.add("activar");
+    console.log("...");
+    }
+    
+    todo_los_bloques.forEach(function(elemento){
+    
+    elemento.addEventListener("click", saludar);
+    
+    })
+    
+    }
+    
+    agregar_eventos();
+    
